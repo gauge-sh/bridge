6f9920d954ec00021b61f1c69e35c9539c733adb
Evan Doyle <evanmdoyle@gmail.com>
2024-04-09 10:30:51 -0700

    Add comments explaining regex

--- bridge/platform/render/redis.py
+++ bridge/platform/render/redis.py
@@ -10,11 +10,14 @@ def build_render_redis_environment() -> RedisEnvironment:
     if not conn_string:
         raise ValueError("REDIS_URL environment variable must be set on Render.")
     redis_env = RedisEnvironment()
+    # Optional password terminated with '@'
+    # Hostname that does not contain ':' or '/'
+    # Optional port number preceded by ':'
+    # Optional database number preceded by '/'
     regex = r"^redis:\/\/(?:(?P<password>[^@]+)@)?(?P<host>[^:\/]+)(?::(?P<port>\d+))?(?:\/(?P<db>\d+))?$"
     match = re.match(regex, conn_string)
     if match:
         components = match.groupdict()
-        # Default port if not specified
         redis_env.host = components.get("host", "localhost")
         redis_env.port = int(components.get("port", 6379))
         redis_env.db = int(components.get("db", 0))
