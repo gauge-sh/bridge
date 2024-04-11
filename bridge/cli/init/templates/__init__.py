c504e6f1320dd8c6e1b5f2fe339143677fa46029
Evan Doyle <evanmdoyle@gmail.com>
2024-04-11 13:43:19 -0700

    Add Deploy to Render button to README in bridge init render

--- bridge/cli/init/templates/__init__.py
+++ bridge/cli/init/templates/__init__.py
@@ -1,5 +1,8 @@
 from bridge.cli.init.templates.build__sh import build_sh_template
 from bridge.cli.init.templates.build_worker__sh import build_worker_sh_template
+from bridge.cli.init.templates.deploy_to_render_button import (
+    deploy_to_render_button_template,
+)
 from bridge.cli.init.templates.render__yaml import render_yaml_template
 from bridge.cli.init.templates.start__sh import start_sh_template
 from bridge.cli.init.templates.start_worker__sh import start_worker_sh_template
@@ -7,6 +10,7 @@ from bridge.cli.init.templates.start_worker__sh import start_worker_sh_template
 __all__ = (
     "build_sh_template",
     "build_worker_sh_template",
+    "deploy_to_render_button_template",
     "render_yaml_template",
     "start_sh_template",
     "start_worker_sh_template",
