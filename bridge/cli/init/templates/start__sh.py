cd40fb2cdabc0c3093c2d3c9ca6a66d245e9b922
Evan Doyle <evanmdoyle@gmail.com>
2024-04-15 17:34:28 -0700

    Use uvicorn workers with gunicorn in start.sh when asgi callable detected

--- bridge/cli/init/templates/start__sh.py
+++ bridge/cli/init/templates/start__sh.py
@@ -1,7 +1,11 @@
+uvicorn_worker_string = "-k uvicorn.workers.UvicornWorker"
+
 template = """#!/usr/bin/env bash
-gunicorn {app_path} -w "${{WEB_CONCURRENCY:-4}}" -b 0.0.0.0:"$PORT"
+gunicorn {app_path} -w "${{WEB_CONCURRENCY:-4}}" -b 0.0.0.0:"$PORT" {worker_string}
 """
 
 
-def start_sh_template(app_path: str) -> str:
-    return template.format(app_path=app_path)
+def start_sh_template(app_path: str, is_asgi: bool = False) -> str:
+    return template.format(
+        app_path=app_path, worker_string=uvicorn_worker_string if is_asgi else ""
+    )
